9876e12b9fcf2cb04329d1429e376559ab7edc8d
VIOT Camille <camille.viot@altran.com>
2018-12-19 18:19:09 +0100

    add more bindings.

--- DOtherSideBindings.cs
+++ DOtherSideBindings.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
 
 namespace DOtherSideDotNet
 {
+    #region type
     public class NamedPtr
     {
         protected IntPtr _ptr;
@@ -20,11 +19,6 @@ namespace DOtherSideDotNet
             returned._ptr = ptr;
             return returned;
         }
-
-        //public static implicit operator IntPtr(NamedPtr ptr)
-        //{
-        //    return new IntPtr(ptr._ptr.ToInt64());
-        //}
     }
 
     public class DosQMetaObject : NamedPtr
@@ -87,16 +81,92 @@ namespace DOtherSideDotNet
         }
     }
 
+    public class DosQUrl : NamedPtr
+    {
+        public static implicit operator DosQUrl(IntPtr ptr)
+        {
+            var returned = new DosQUrl();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQQuickView : NamedPtr
+    {
+        public static implicit operator DosQQuickView(IntPtr ptr)
+        {
+            var returned = new DosQQuickView();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQHashIntByteArray : NamedPtr
+    {
+        public static implicit operator DosQHashIntByteArray(IntPtr ptr)
+        {
+            var returned = new DosQHashIntByteArray();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQModelIndex : NamedPtr
+    {
+        public static implicit operator DosQModelIndex(IntPtr ptr)
+        {
+            var returned = new DosQModelIndex();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQAbstractItemModel : NamedPtr
+    {
+        public static implicit operator DosQAbstractItemModel(IntPtr ptr)
+        {
+            var returned = new DosQAbstractItemModel();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQAbstractTableModel : NamedPtr
+    {
+        public static implicit operator DosQAbstractTableModel(IntPtr ptr)
+        {
+            var returned = new DosQAbstractTableModel();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+
+    public class DosQAbstractListModel : NamedPtr
+    {
+        public static implicit operator DosQAbstractListModel(IntPtr ptr)
+        {
+            var returned = new DosQAbstractListModel();
+            returned._ptr = ptr;
+            return returned;
+        }
+    }
+    #endregion
+
     public unsafe static class DOtherSideBindings
     {
         public const string DllName = @"DOtherSide.dll";
 
+        #region CharArray
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dos_chararray_delete(sbyte* ptr);
+        #endregion
 
+        #region QCoreApplication
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern sbyte* dos_qcoreapplication_application_dir_path();
+        #endregion
 
+        #region QApplication
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dos_qapplication_create();
 
@@ -108,7 +178,9 @@ namespace DOtherSideDotNet
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dos_qapplication_delete();
+        #endregion
 
+        #region QGuiApplication
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dos_qguiapplication_create();
 
@@ -120,38 +192,91 @@ namespace DOtherSideDotNet
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void dos_qguiapplication_delete();
+        #endregion
 
+        #region QQmlContext
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         private static extern void dos_qqmlcontext_setcontextproperty
         (
-            IntPtr vptr, 
-            [MarshalAs(UnmanagedType.LPStr)] string name, 
+            IntPtr vptr,
+            [MarshalAs(UnmanagedType.LPStr)] string name,
             IntPtr value
         );
 
-       internal static void dos_qqmlcontext_setcontextproperty
-       (
-           DosQQmlContext vptr,
-           string name,
-           DosQVariant value
-       )
-       {
+        internal static void dos_qqmlcontext_setcontextproperty
+        (
+            DosQQmlContext vptr,
+            string name,
+            DosQVariant value
+        )
+        {
             dos_qqmlcontext_setcontextproperty
             (
                 vptr.Ptr,
                 name,
                 value.Ptr
             );
-       }
+        }
+        #endregion
 
+        #region QQmlApplicationEngine
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_create")]
         private static extern IntPtr DosQqmlApplicationengineCreate();
 
         internal static DosQQmlApplicationEngine dos_qqmlapplicationengine_create()
         {
-            IntPtr returnedPtr = DosQqmlApplicationengineCreate();
-            DosQQmlApplicationEngine returned = returnedPtr;
-            return returned;
+            return DosQqmlApplicationengineCreate();
         }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_load")]
+        private static extern void DosQqmlapplicationengineLoad(IntPtr vptr, [MarshalAs(UnmanagedType.LPStr)] string filename);
+
+        internal static void dos_qqmlapplicationengine_load(DosQQmlApplicationEngine engine, [MarshalAs(UnmanagedType.LPStr)] string filename)
+        {
+            DosQqmlapplicationengineLoad(engine.Ptr, filename);
+        }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_load_url")]
+        private static extern void DosQqmlapplicationengineLoadUrl(IntPtr vptr, IntPtr url);
+
+        internal static void dos_qqmlapplicationengine_load_url(DosQQmlApplicationEngine engine, DosQUrl url)
+        {
+            DosQqmlapplicationengineLoadUrl(engine.Ptr, url.Ptr);
+        }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_load_data")]
+        private static extern void DosQqmlapplicationengineLoadData(IntPtr vptr, [MarshalAs(UnmanagedType.LPStr)] string data);
+
+        internal static void dos_qqmlapplicationengine_load_data(DosQQmlApplicationEngine engine, string data)
+        {
+            DosQqmlapplicationengineLoadData(engine.Ptr, data);
+        }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_add_import_path")]
+        private static extern void DosQqmlapplicationengineAddImportPath(IntPtr vptr, [MarshalAs(UnmanagedType.LPStr)] string path);
+
+        internal static void dos_qqmlapplicationengine_add_import_path(DosQQmlApplicationEngine engine, string path)
+        {
+            DosQqmlapplicationengineAddImportPath(engine.Ptr, path);
+        }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_context")]
+        private static extern IntPtr DosQqmlapplicationengineContext(IntPtr vptr);
+
+        internal static DosQQmlContext dos_qqmlapplicationengine_context(DosQQmlApplicationEngine engine)
+        {
+            return DosQqmlapplicationengineContext(engine.Ptr);
+        }
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "dos_qqmlapplicationengine_delete")]
+        private static extern void DosQqmlapplicationengineDelete(IntPtr vptr);
+
+        internal static void dos_qqmlapplicationengine_delete(DosQQmlApplicationEngine engine)
+        {
+            DosQqmlapplicationengineDelete(engine.Ptr);
+        }
+        #endregion
+
+
     }
 }
